c6353cdc112c156ed7762a2c7a63fda409a6d051
Carl Timothy Tolentino <carl.tolentino.52@gmail.com>
2025-05-30 13:30:49 +0100

    randomized initial orientation and saves user pose history

---
+++ UnityProjects/MRTKDevTemplate/Assets/Scripts/OwnScripts/PoseLogger.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PoseLogger : MonoBehaviour
+{
+    public Transform xrCamera; // Assign XR Camera (Main Camera)
+    private List<string> buffer = new List<string>();
+    private string filePath;
+
+    void Start()
+    {
+        string fileName = "UserPoseLog_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".csv";
+        filePath = Path.Combine(Application.persistentDataPath, fileName);
+
+        // Add CSV header
+        buffer.Add("Time,PosX,PosY,PosZ,RotX,RotY,RotZ");
+
+        Debug.Log("Logging to CSV file on exit:\n" + filePath);
+    }
+
+    void Update()
+    {
+        if (xrCamera == null) return;
+
+        Vector3 pos = xrCamera.position;
+        Quaternion rot = xrCamera.rotation;
+        Vector3 euler = rot.eulerAngles;
+
+
+        string line = string.Format("{0:F3},{1:F4},{2:F4},{3:F4},{4:F4},{5:F4},{6:F4}",
+            Time.time, pos.x, pos.y, pos.z, euler.x, euler.y, euler.z);
+
+        buffer.Add(line);
+
+        }
+
+    void OnApplicationPause()
+    {
+        File.WriteAllLines(filePath, buffer);
+    }
+}
